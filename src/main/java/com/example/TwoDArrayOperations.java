749a10faf7c6b680266316d1d1df7288617ca522
rentsdue <hoganwong88@gmail.com>
2023-12-04 13:33:19 +0800

    partly done

--- src/main/java/com/example/TwoDArrayOperations.java
+++ src/main/java/com/example/TwoDArrayOperations.java
@@ -3,20 +3,33 @@ package com.example;
 public class TwoDArrayOperations {
 
     public static void main(String[] args) {
-        // Exercise 1: Initialize and Print a 2D Array
-        // TODO: Initialize a 2D array of integers and print its contents in a matrix form.
+        int [][] array= {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        System.out.println(array);
     }
 
     // Exercise 2: Find the Maximum Value in a 2D Array
     public static int findMaximumValue(int[][] array) {
-        // TODO: Implement the logic to find the maximum value in the 2D array.
-        return 0; // Placeholder return value
+        int max= array[0][0];
+        for (int i=0; i<array.length; i++) {
+            for (int j=0; j<array[i].length; j++) {
+                if (array[i][j]>max) {
+                    max=array[i][j];
+                }
+            }
+        }
+        return max; 
     }
 
     // Exercise 3: Check for a Specific Value in a 2D Array
     public static boolean containsValue(int[][] array, int value) {
-        // TODO: Implement the logic to check if the 2D array contains the specified value.
-        return false; // Placeholder return value
+        for (int i=0; i<array.length; i++) {
+            for (int num: array[i]) {
+                if (value==num) {
+                    return true;
+                }
+            }
+        }
+        return false; 
     }
 
     // Exercise 4: Calculate the Average of All Elements in a 2D Array
